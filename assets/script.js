965c9283fe2dbeb17b1c79265430bb741eb86ee5
Brock Williams <b.williams54255@gmail.com>
2023-08-17 23:15:51 -0700

    Added a display question and shufle array and check answer functions.

--- assets/script.js
+++ assets/script.js
@@ -103,3 +103,70 @@ const quizData = [
         quizContainer.appendChild(optionsElement);
         quizContainer.appendChild(submitButton);
       }
+
+      function checkAnswer() {
+        const selectedOption = document.querySelector('input[name="quiz"]:checked');
+        if (selectedOption) {
+          const answer = selectedOption.value;
+          if (answer === quizData[currentQuestion].answer) {
+            score++;
+          } else {
+            incorrectAnswers.push({
+              question: quizData[currentQuestion].question,
+              incorrectAnswer: answer,
+              correctAnswer: quizData[currentQuestion].answer,
+            });
+            timeLeft -= 10; // Subtract 10 seconds for incorrect answer
+          }
+          currentQuestion++;
+          selectedOption.checked = false;
+          if (currentQuestion < quizData.length) {
+            displayQuestion();
+          } else {
+            displayResult();
+          }
+        }
+      }
+      function displayResult() {
+        clearInterval(countdown);
+        quizContainer.style.display = 'none';
+        submitButton.style.display = 'none';
+        retryButton.style.display = 'inline-block';
+        showAnswerButton.style.display = 'inline-block';
+        
+        const initialsInput = document.createElement('input');
+        initialsInput.type = 'text';
+        initialsInput.id = 'initialsInput';
+        initialsInput.placeholder = 'Enter your initials';
+        
+        const highScoresButton = document.createElement('button');
+        highScoresButton.className = 'button';
+        highScoresButton.textContent = 'High Scores';
+        highScoresButton.id = 'highScores';
+        highScoresButton.addEventListener('click', displayHighScores);
+        
+        resultContainer.innerHTML = `You scored ${score} out of ${quizData.length}!<br>Time left: ${timeLeft}`;
+        resultContainer.appendChild(initialsInput);
+        resultContainer.appendChild(highScoresButton);
+      }
+      
+      function retryQuiz() {
+        clearInterval(countdown);
+        currentQuestion = 0;
+        score = 0;
+        timeLeft = 60; // Reset the timer
+        incorrectAnswers = [];
+        quizContainer.style.display = 'block';
+        submitButton.style.display = 'inline-block';
+        retryButton.style.display = 'none';
+        showAnswerButton.style.display = 'none';
+        
+        const initialsInput = document.getElementById('initialsInput');
+        if (initialsInput) {
+          initialsInput.value = ''; // Reset initials input
+        }
+        
+        resultContainer.innerHTML = '';
+        displayQuestion();
+      }
+      
