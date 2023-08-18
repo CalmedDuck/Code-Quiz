2ec85308cc13dc676b03fe8779eadd213b416eb7
Brock Williams <b.williams54255@gmail.com>
2023-08-17 23:07:23 -0700

    Added questions to Javascript.

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+const quizData = [
+    {
+      question: 'Inside which HTML element do we put JavaScript?',
+      options: ['<scripting>', '<script>', '<javascript>', '<js>'],
+      answer: '<scripting>',
+    },
+    {
+      question: 'What is the correct place to insert a JavaScript?',
+      options: ['The <body> section', 'Both the <head> section and the <body> section are correct', 'The <head> section', 'Neptune'],
+      answer: 'Both the <head> section and the <body> section are correct',
+    },
+    {
+      question: 'How do you call a function named myFunction?',
+      options: ['myFunction()', 'call function myFunction()', 'call myFunction()', 'Argentina'],
+      answer: 'myFunction()',
+    },
+    {
+      question: 'How does a WHILE loop start?',
+      options: ['while (i <= 10)', 'while i = 1 to 10', 'while (i <= 10, i++', 'Idk'],
+      answer: 'while (i <= 10)',
+    },
+    {
+      question: 'Which is the largest ocean on Earth?',
+      options: [
+        'Pacific Ocean',
+        'Indian Ocean',
+        'Atlantic Ocean',
+        'Arctic Ocean',
+      ],
+      answer: 'Pacific Ocean',
+    },
+
+    ];
